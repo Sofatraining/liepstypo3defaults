4556678dfe39f65eaf50295661e92a596c52749d
Jürgen Ohnesorge <47626641+Sofatraining@users.noreply.github.com>
2022-02-17 12:47:50 +0100

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,9 +3,9 @@ defined('TYPO3_MODE') || die();
 /***************
  * Add default RTE configuration
  */
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['lieps_typo3_defaults'] = 'EXT:lieps_typo3_defaults/Configuration/RTE/Default.yaml';
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['liepstypo3defaults'] = 'EXT:liepstypo3defaults/Configuration/RTE/Default.yaml';
 
 /***************
  * PageTS
  */
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:lieps_typo3_defaults/Configuration/TsConfig/Page/All.tsconfig">');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:liepstypo3defaults/Configuration/TsConfig/Page/All.tsconfig">');
