58b89007de1e1422247dcde6f88dc7ebde60be31
Jürgen Ohnesorge <47626641+Sofatraining@users.noreply.github.com>
2022-02-17 20:59:35 +0100

    Add files via upload

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,20 +1,8 @@
 <?php
-defined('TYPO3') or die('Access denied.');
-call_user_func(function()
-{
-    /**
-     * Temporary variables
-     */
-    $extensionKey = 'liepstypo3defaults';
+if (!defined('TYPO3_MODE')) {
+	die ('Access denied.');
+}
 
-    /**
-     * Default TypoScript for LiepsTypo3Defaults
-     */
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-        $extensionKey,
-        'Configuration/TypoScript',
-        'LIEPS TYPO3 Defaults'
-    );
-});
+
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('liepstypo3defaults', 'Configuration/TypoScript', 'LIEPS TYPO3 Defaults');
