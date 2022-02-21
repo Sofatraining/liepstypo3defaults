6639478387f6d0a626759f7c780cc0b5667072e5
Jürgen Ohnesorge <47626641+Sofatraining@users.noreply.github.com>
2022-02-21 14:08:22 +0100

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,6 +4,27 @@ defined('TYPO3_MODE') || die();
  * Add default RTE configuration
  */
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['liepstypo3defaults'] = 'EXT:liepstypo3defaults/Configuration/RTE/Default.yaml';
+// Register FormEngine node type resolver hook to render RTE in FormEngine if enabled
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['formEngine']['nodeResolver'][1480314091] = [
+    'nodeName' => 'text',
+    'priority' => 50,
+    'class' => \TYPO3\CMS\RteCKEditor\Form\Resolver\RichTextNodeResolver::class,
+];
+
+if (TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_BE) {
+    \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
+        \TYPO3\CMS\Core\Page\PageRenderer::class
+    )->addRequireJsConfiguration([
+        'shim' => [
+            'ckeditor' => ['exports' => 'CKEDITOR']
+        ],
+        'paths' => [
+            'ckeditor' => \TYPO3\CMS\Core\Utility\PathUtility::getAbsoluteWebPath(
+                \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('rte_ckeditor', 'Resources/Public/JavaScript/Contrib/')
+            ) . 'ckeditor'
+        ]
+    ]);
+}
 
 /***************
  * PageTS
