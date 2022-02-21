17dcdf61eba36881b42657a51e207c58f6c140d4
Jürgen Ohnesorge <47626641+Sofatraining@users.noreply.github.com>
2022-02-21 14:13:51 +0100

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -5,21 +5,6 @@ defined('TYPO3_MODE') || die();
  */
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['liepstypo3defaults'] = 'EXT:liepstypo3defaults/Configuration/RTE/Default.yaml';
 
-if (TYPO3_REQUESTTYPE & TYPO3_REQUESTTYPE_BE) {
-    \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(
-        \TYPO3\CMS\Core\Page\PageRenderer::class
-    )->addRequireJsConfiguration([
-        'shim' => [
-            'ckeditor' => ['exports' => 'CKEDITOR']
-        ],
-        'paths' => [
-            'ckeditor' => \TYPO3\CMS\Core\Utility\PathUtility::getAbsoluteWebPath(
-                \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('rte_ckeditor', 'Resources/Public/JavaScript/Contrib/')
-            ) . 'ckeditor'
-        ]
-    ]);
-}
-
 /***************
  * PageTS
  */
