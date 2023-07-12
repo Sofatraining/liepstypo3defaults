340ea6abb595478f3fbffbfbf4d0e7a82a2e14d3
Jürgen Ohnesorge <47626641+Sofatraining@users.noreply.github.com>
2023-07-12 14:06:24 +0200

    Update custom.js

--- Resources/Public/Js/custom.js
+++ Resources/Public/Js/custom.js
@@ -1,19 +1,3 @@
-//This simple script handles the header nav menu modal
-//also covered nav sub menu selection
-//Dependencies: jQuery
-$(document).ready(function() {
-	$('.ham').click(function(){
-		$('header').toggleClass('clicked');
-	});
-
-	$('nav ul li').click(function(){
-		$('nav ul li').removeClass('selected');
-		$('nav ul li').addClass('notselected');
-		$(this).toggleClass('selected');
-		$(this).removeClass('notselected');
-	});	
-});
-
 // Magnific Popup
 $(document).ready(function() {
 	$('.image-link').magnificPopup({
