97f67f0499f11d353b455ec2f0d64b051ff50580
Jürgen Ohnesorge <47626641+Sofatraining@users.noreply.github.com>
2023-09-20 15:34:55 +0200

    Delete Classes/Controller/PageController.php

--- Classes/Controller/PageController.php
+++
@@ -1,8 +0,0 @@
-<?php
-declare(strict_types=1);
-namespace LiepsGmbH\Liepstypo3defaults\Controller;
-use FluidTYPO3\Flux\Controller\PageController as AbstractController;
-
-class PageController extends AbstractController
-{
-}
