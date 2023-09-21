6312afe3fbf6409ed30039d40b771b47fb01b02c
Jürgen Ohnesorge <47626641+Sofatraining@users.noreply.github.com>
2023-09-21 11:43:19 +0200

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,3 +9,6 @@ $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['liepstypo3defaults'] = 'EXT:lieps
  * PageTS
  */
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:liepstypo3defaults/Configuration/TsConfig/Page/All.tsconfig">');
+
+\FluidTYPO3\Flux\Core::registerProviderExtensionKey('LiepsGmbH.Liepstypo3defaults', 'Page');
+\FluidTYPO3\Flux\Core::registerProviderExtensionKey('LiepsGmbH.Liepstypo3defaults', 'Content');
