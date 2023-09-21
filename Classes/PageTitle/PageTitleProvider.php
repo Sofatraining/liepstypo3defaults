754beb33d6c8a6df0c44aded21e3e8740579f482
Jürgen Ohnesorge <47626641+Sofatraining@users.noreply.github.com>
2023-09-21 10:43:05 +0200

    Create PageTitleProvider.php

---
+++ Classes/PageTitle/PageTitleProvider.php
@@ -0,0 +1,16 @@
+<?php
+namespace LiepsGmbH\Liepstypo3defaults\PageTitle;
+
+use TYPO3\CMS\Core\PageTitle\AbstractPageTitleProvider;
+
+class LiepsPageTitleProvider extends AbstractPageTitleProvider
+{
+	public function __construct()
+	{	
+		$this->title = '';
+		$page = $GLOBALS['TSFE']->page;
+		$separator = ' :';
+		$this->title .= $page['title'];
+		$this->title .= $separator;
+	}
+}
