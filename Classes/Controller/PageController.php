36a2b2eab0bfa8f43fe98970f836e2626a8a9d0d
Jürgen Ohnesorge <47626641+Sofatraining@users.noreply.github.com>
2023-09-21 12:02:21 +0200

    Update PageController.php

--- Classes/Controller/PageController.php
+++ Classes/Controller/PageController.php
@@ -1,8 +1,8 @@
 <?php
-declare(strict_types=1);
 namespace LiepsGmbH\Liepstypo3defaults\Controller;
 use FluidTYPO3\Flux\Controller\PageController as AbstractController;
 
 class PageController extends AbstractController
 {
+
 }
