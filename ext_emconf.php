18344efd800991f7052c55e62479baf91619b583
Jürgen Ohnesorge <47626641+Sofatraining@users.noreply.github.com>
2024-11-15 11:03:36 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,9 +20,9 @@ $EM_CONF[$_EXTKEY] = array (
         'depends' => 
             array (
                 'typo3' => '11.5.0-12.4.99',
-                'content_blocks' => '0.7.14-0.7.99',
+                'content_blocks' => '0.7.14-1.0.99',
                 'flux' => '10.0.10-10.0.99',
-                'vhs' => '7.0.3-7.0.99',
+                'vhs' => '7.0.7-7.0.99',
             ),
         'conflicts' => 
             array (
