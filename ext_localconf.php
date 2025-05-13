18d75a5440327e45a5b114b7500dc0ca9b27d2c6
Jürgen Ohnesorge <47626641+Sofatraining@users.noreply.github.com>
2025-05-13 17:29:27 +0200

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -13,8 +13,9 @@ $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['liepstypo3defaults'] = 'EXT:lieps
 \FluidTYPO3\Flux\Core::registerProviderExtensionKey('LiepsGmbH.Liepstypo3defaults', 'Page');
 \FluidTYPO3\Flux\Core::registerProviderExtensionKey('LiepsGmbH.Liepstypo3defaults', 'Content');
 
-// Setzen der Konstante [GFX][imagefile_ext]
+// Setzen von Konstanten
 $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext'] = 'gif,jpg,jpeg,tif,tiff,bmp,pcx,tga,png,pdf,ai,svg,webp';
 $GLOBALS['TYPO3_CONF_VARS']['BE']['passwordReset'] = 'false';
 $GLOBALS['TYPO3_CONF_VARS']['BE']['passwordResetForAdmins'] = 'false';
 $GLOBALS['TYPO3_CONF_VARS']['SYS']['systemLocale'] = 'de_DE.utf8';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['locales']['default'] = 'de_DE';
