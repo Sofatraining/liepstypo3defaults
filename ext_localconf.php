785b277140469d8f1ad365a0368963de140bef9a
Jürgen Ohnesorge <47626641+Sofatraining@users.noreply.github.com>
2025-05-13 17:28:55 +0200

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -15,4 +15,6 @@ $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['liepstypo3defaults'] = 'EXT:lieps
 
 // Setzen der Konstante [GFX][imagefile_ext]
 $GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext'] = 'gif,jpg,jpeg,tif,tiff,bmp,pcx,tga,png,pdf,ai,svg,webp';
-$GLOBALS['TYPO3_CONF_VARS']['SYS']['locales']['default'] = 'de_DE';
+$GLOBALS['TYPO3_CONF_VARS']['BE']['passwordReset'] = 'false';
+$GLOBALS['TYPO3_CONF_VARS']['BE']['passwordResetForAdmins'] = 'false';
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['systemLocale'] = 'de_DE.utf8';
