aaaaa069848e62dd10ccc9581f711266d6be48f7
Jürgen Ohnesorge <47626641+Sofatraining@users.noreply.github.com>
2025-09-18 11:16:09 +0200

    Refactor Magnific Popup initialization and callbacks

--- Resources/Public/Js/custom.js
+++ Resources/Public/Js/custom.js
@@ -7,16 +7,56 @@ console.log(
 );
 
 // Magnific Popup
-$(document).ready(function() {
-	$('.image-link').magnificPopup({
-		type:'image',
-		gallery: {
-			enabled: true
-		},
-		image: {
-			titleSrc: function(item) {
-				return item.el.find('img').attr('title');
-			}
-		}
-	});
+$(function () {
+  $('body').magnificPopup({
+    delegate: 'a.mfp-link',
+    type: 'image',
+    gallery: { enabled: true },
+    callbacks: {
+      elementParse: function (item) {
+        var $el = item.el;
+        var href = $el.attr('href') || '';
+        var dataType = $el.attr('data-type');
+
+        if (
+          dataType === 'iframe' ||
+          /youtube\.com|youtu\.be|youtube-nocookie\.com|vimeo\.com/i.test(href)
+        ) {
+          item.type = 'iframe';
+        } else {
+          item.type = 'image';
+        }
+      }
+    },
+    image: {
+      titleSrc: function (item) {
+        // zuerst Titel aus <img>, fallback auf <a title="">
+        return (
+          item.el.find('img').attr('title') ||
+          item.el.attr('title') ||
+          ''
+        );
+      }
+    },
+    iframe: {
+      patterns: {
+        youtube: {
+          index: 'youtube.com/',
+          id: function (url) {
+            var m = url.match(/[?&]v=([^?&]+)/);
+            return m && m[1] ? m[1] : null;
+          },
+          src: 'https://www.youtube-nocookie.com/embed/%id%?autoplay=1&rel=0'
+        },
+        youtu_be: {
+          index: 'youtu.be/',
+          id: function (url) {
+            var m = url.match(/youtu\.be\/([^?&/]+)/);
+            return m && m[1] ? m[1] : null;
+          },
+          src: 'https://www.youtube-nocookie.com/embed/%id%?autoplay=1&rel=0'
+        }
+      }
+    }
+  });
 });
