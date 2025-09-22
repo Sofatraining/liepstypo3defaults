79bc55de59cac8929139c8fce2d16a7ece8fa524
Jürgen Ohnesorge <47626641+Sofatraining@users.noreply.github.com>
2025-09-22 10:28:40 +0200

    Update flux dependency version to 11.0.7

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array (
             array (
                 'typo3' => '13.4.10-13.4.99',
                 'content_blocks' => '1.3.14-1.3.99',
-                'flux' => '11.0.5-11.1.99',
+                'flux' => '11.0.7-11.1.99',
                 'vhs' => '7.1.4-7.1.99',
             ),
         'conflicts' => 
