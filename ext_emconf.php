9572e0e2228edfc8cc2b56aa9a363b13211fe300
Jürgen Ohnesorge <47626641+Sofatraining@users.noreply.github.com>
2025-11-04 10:46:27 +0100

    Update dependencies for content_blocks and flux

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,8 +20,8 @@ $EM_CONF[$_EXTKEY] = array (
         'depends' => 
             array (
                 'typo3' => '13.4.10-13.4.99',
-                'content_blocks' => '1.3.14-1.3.99',
-                'flux' => '11.0.7-11.1.99',
+                'content_blocks' => '1.3.16-1.3.99',
+                'flux' => '11.1.0-11.1.99',
                 'vhs' => '7.1.4-7.1.99',
             ),
         'conflicts' => 
